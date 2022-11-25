3c5d9e448a51bcf09720bfd2ee87f24172c59663
Sachin Rao <sachinrao94450@gmail.com>
2022-11-25 17:06:26 +0530

    object added

--- src/component/Object.js
+++ src/component/Object.js
@@ -2,7 +2,7 @@
 export const userDetail = [
   {
     username: [{
-      personalInfo: { name: "dev", address: "Noida", phoneNo: "12323123" },
+      personalInfo: { name: "dev", address: "Delhi", phoneNo: "12323123" },
       professional: { experience: "3 Years" },
       academic: {
         college: "Sms", degree: "B.teh", passOut: "2022-06- 12", address: "Lucknow ",
