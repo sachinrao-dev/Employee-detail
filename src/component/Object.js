ac8b1dbe74d4fedda60fc2c8e9ce2711771f54e6
Sachin Rao <sachinrao9450@gmail.com>
2022-11-26 15:03:54 +0530

    implement in object

--- src/component/Object.js
+++ src/component/Object.js
@@ -1,32 +1,28 @@
 /* eslint-disable import/prefer-default-export */
-const employeeDetail = [{
-  employee: {
-    name: "John",
-    dateOfBirth: "22/02/2022",
-    occupation: "developer",
-    personal: {
-      name: "john", address: "Delhi", phoneNo: 1234341,
-    },
-    professional: { experiences: "3 years" },
-    academic: {
-      collegeName: "Sms ", degree: "B.tech", passOut: "2022-06-22", address: "Lucknow",
-    },
-    employment: { currentCompany: "Celestial" },
+export const employeeDetail = [{
+  name: "John",
+  dateOfBirth: "22/02/2022",
+  occupation: "developer",
+  personal: {
+    name: "john", address: "Delhi", phoneNo: 1234341,
   },
+  professional: { experiences: "3 years" },
+  academic: {
+    collegeName: "Sms ", degree: "B.tech", passOut: "2022-06-22", address: "Lucknow",
+  },
+  employment: { currentCompany: "Celestial" },
 },
 {
-  employee: {
-    name: "John",
-    dateOfBirth: "22/02/2022",
-    occupation: "developer",
-    personal: {
-      name: "john", address: "Delhi", phoneNo: 1234341,
-    },
-    professional: { experiences: "3 years" },
-    academic: {
-      collegeName: "Sms ", degree: "B.tech", passOut: "2022-06-22", address: "Lucknow",
-    },
-    employment: { currentCompany: "Celestial" },
+  name: "John",
+  dateOfBirth: "22/02/2022",
+  occupation: "developer",
+  personal: {
+    name: "john", address: "Delhi", phoneNo: 1234341,
   },
+  professional: { experiences: "3 years" },
+  academic: {
+    collegeName: "Sms ", degree: "B.tech", passOut: "2022-06-22", address: "Lucknow",
+  },
+  employment: { currentCompany: "Celestial" },
 },
 ];
